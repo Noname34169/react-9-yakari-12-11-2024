936830968f313c107e680a0d32f82b8df8850dc6
Noname34169 <salomovasil0@gmail.com>
2024-11-12 20:53:23 +0500

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Hero.scss'
+
+const Hero = ({num}) => {
+  return (
+    <>
+    <section className={`hero hero${num}`} id={`hero${num}`}>
+        <div className="container">
+            <h1 className="hero__title">SECTION {num}</h1>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Hero
